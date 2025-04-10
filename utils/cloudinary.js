60263848c9c168ac1c1bce3acaa123b489e53526
mustafafarag <mustafafarag23@gmail.com>
2025-04-10 11:30:34 +0200

    refactor: Clean up Cloudinary configuration and improve code readability

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,30 +1,26 @@
+// Importing the Cloudinary library
 const cloudinary = require('cloudinary');
 
 
 
 // Initialize Cloudinary with your credentials
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Cloudinary cloud name from environment variables
+  api_key: process.env.CLOUDINARY_API_KEY, // Cloudinary API key from environment variables
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Cloudinary API secret from environment variables
+  secure: true, // Use secure URLs
 });
 
-
-// Function to upload an image to Cloudinary
-// and return the URL and resource type
 const cloudinaryUploadImage = async (filePath) => {
   return new Promise((resolve, reject) => {
     cloudinary.v2.uploader.upload(filePath, (error, result) => {
       if (error) {
-        reject(error);
+        reject(error); // Reject the promise if an error occurs
       } else {
-        resolve({url : result.secure_url, ressource_type: "auto"});
+        resolve({ url: result.secure_url, ressource_type: "auto" }); // Resolve the promise with the uploaded image URL
       }
     });
   });
 };
 
-
-
-module.exports = { cloudinaryUploadImage,};
+module.exports = { cloudinaryUploadImage }; // Export the cloudinaryUploadImage function
