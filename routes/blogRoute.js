a8dc5c864fd456fde5c81d4ae90b3cc7397c51ee
mustafafarag <mustafafarag23@gmail.com>
2025-04-13 10:38:42 +0200

    feat: Add uploadImages functionality to blog routes

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createBlog, updateBlog ,getBlog , getAllBlogs, deleteBlog, likeBlog , dislikeBlog} = require("../controller/blogCtrl");
+const { createBlog, updateBlog ,getBlog , getAllBlogs, deleteBlog, likeBlog , dislikeBlog, uploadImages} = require("../controller/blogCtrl");
 const { authmiddleware , isAdmin } = require("../middlewares/authmiddleware");
+const { uploadPhoto, blogsImgResize } = require("../middlewares/uploadImages");
 
 
 router.post("/", authmiddleware, isAdmin, createBlog);
+router.put("/upload/:id", authmiddleware, isAdmin , uploadPhoto.array("images", 10), blogsImgResize, uploadImages);
 router.put("/likes", authmiddleware,isAdmin, likeBlog);
 router.put("/dislikes", authmiddleware,isAdmin, dislikeBlog);
 router.put("/:id", authmiddleware, isAdmin, updateBlog);
