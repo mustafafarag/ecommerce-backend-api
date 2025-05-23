433bbdc4b99ec24537c9e81d0b460f3c97b31bf9
mustafafarag <mustafafarag23@gmail.com>
2025-05-23 06:55:28 +0300

    fix: update MongoDB connection URI environment variable from MONGODB_URL to MONGODB_URI & - Configured MongoDB authentication and authorization using SCRAM (Salted Challenge Response Authentication Mechanism).

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const dbConnect = () => {
 
     try{
-        const conn = mongoose.connect(process.env.MONGODB_URL)
+        const conn = mongoose.connect(process.env.MONGODB_URI)
         console.log("DB Connected Successfully")
 
     } catch(error){
